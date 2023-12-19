7cd29d9ea845db543751550ed4fc9530a0288e0e
Nigarglm <tu7mk3dbf@code.edu.az>
2023-12-20 02:07:21 +0400

    onion added

---
+++ src/Core/ProniaOnion.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProniaOnion.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set;}
+        public string CreatedBy { get; set; }
+        public BaseEntity()
+        {
+            CreatedAt = DateTime.Now;
+            CreatedBy = "nigar.glm";
+        }
+    }
+}
---
+++ src/Core/ProniaOnion.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProniaOnion.Domain.Entities
+{
+    public class Product:BaseNameableEntity
+    {
+        public decimal Price { get; set; }
+        public string SKU { get; set; } = null!;
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ICollection<ProductColor>? ProductColors { get; set; }
+    }
+}
